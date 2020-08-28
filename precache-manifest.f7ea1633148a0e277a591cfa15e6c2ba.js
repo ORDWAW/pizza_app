8bb61a1f394bc587e7dc1349104b165ef5ad6e3d
Artur Januszkiewicz <65866148+ORDWAW@users.noreply.github.com>
2020-08-28 17:56:27 +0200

    Updates

---
+++ precache-manifest.f7ea1633148a0e277a591cfa15e6c2ba.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e74c8914aa9a48cb377bcf2437411f",
+    "url": "/pizza_app/index.html"
+  },
+  {
+    "revision": "5fccfcfcd9e9999bdfd4",
+    "url": "/pizza_app/static/css/main.5026615a.chunk.css"
+  },
+  {
+    "revision": "ccda28dd834a8503483d",
+    "url": "/pizza_app/static/js/2.9f6f5e50.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pizza_app/static/js/2.9f6f5e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fccfcfcd9e9999bdfd4",
+    "url": "/pizza_app/static/js/main.ac138b78.chunk.js"
+  },
+  {
+    "revision": "6d31284585f1f3a2705f",
+    "url": "/pizza_app/static/js/runtime-main.4f76949f.js"
+  },
+  {
+    "revision": "429523437773ebc730cc9f137fea2427",
+    "url": "/pizza_app/static/media/chicago_style.42952343.jpeg"
+  },
+  {
+    "revision": "0b6ad0f8fcb560ce4be213384291a55d",
+    "url": "/pizza_app/static/media/final_contact_gif.0b6ad0f8.gif"
+  },
+  {
+    "revision": "649592502ac7a63f0f6b3e516e157c00",
+    "url": "/pizza_app/static/media/just_text_logo.64959250.png"
+  },
+  {
+    "revision": "bde3b3134e3755a5ca44b7cbf9f08a0e",
+    "url": "/pizza_app/static/media/margarita.bde3b313.jpg"
+  },
+  {
+    "revision": "6d9f4aa6866b4ca7e66a0107d26298c4",
+    "url": "/pizza_app/static/media/menu_gif.6d9f4aa6.gif"
+  },
+  {
+    "revision": "e85c11c524deae9724ac5931c2f29590",
+    "url": "/pizza_app/static/media/pepperoni.e85c11c5.jpeg"
+  },
+  {
+    "revision": "daab15a2f46b977951325014f4107c7d",
+    "url": "/pizza_app/static/media/real_final_gif.daab15a2.gif"
+  },
+  {
+    "revision": "f45f8765f2dabcf13b02bcb8ab85e4e7",
+    "url": "/pizza_app/static/media/seasonal.f45f8765.jpeg"
+  },
+  {
+    "revision": "2e092e643b483abd1a95e08f84615a1f",
+    "url": "/pizza_app/static/media/vegan.2e092e64.jpeg"
+  }
+]);
